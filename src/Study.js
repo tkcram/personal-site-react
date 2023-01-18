b3ac34522c389ad263c677ce025a48a8118ecff4
tkcram <100544183+tkcram@users.noreply.github.com>
2023-01-17 21:57:11 -0500

    more build fixes

--- src/Study.js
+++ src/Study.js
@@ -1,4 +1,4 @@
-import useParams from 'react-router-dom' 
+import { useParams } from 'react-router-dom' 
 import Header from './Header'
 import Footer from './Footer'
 import caseStudies from './case-studies'
